801a4041ae601db0e4ba38a73ce9da9179575d9b
Vikram Biwal <vikram@punchh.com>
2021-08-31 17:45:43 +0530

    initial commit

--- loop_example.go
+++ loop_example.go
@@ -1,35 +1,3 @@
-# simple example
-
-```go
-package main
-
-import (
-"os"
-"text/template"
-)
-
-type Todo struct {
-  Name string
-  Description string
-}
-
-func main() {
-  td := Todo{"Test templates", "Let's test a template to see the magic."}
-
-  t, err := template.New("todos").Parse("You have a task named \"{{ .Name}}\" with description: \"{{ .Description}}\"")
-  if err != nil {
-    panic(err)
-  }
-  err = t.Execute(os.Stdout, td)
-  if err != nil {
-    panic(err)
-  }
-}
-```
-
-# loop example
-
-```go
 package main
 
 import (
@@ -72,5 +40,3 @@ func main() {
 		panic(err)
 	}
 }
-
-```
---
+++ simple_example.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+type Todo struct {
+	Name        string
+	Description string
+}
+
+func main() {
+	td := Todo{"Test templates", "Let's test a template to see the magic."}
+
+	t, err := template.New("todos").Parse("You have a task named \"{{ .Name}}\" with description: \"{{ .Description}}\"")
+	if err != nil {
+		panic(err)
+	}
+	err = t.Execute(os.Stdout, td)
+	if err != nil {
+		panic(err)
+	}
+}
